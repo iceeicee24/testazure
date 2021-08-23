b9bf8ae23949e81640d44f081af5b4dd46f0ee4c
isaac viray <isaac.j.viray@gmail.com>
2021-08-23 10:08:58 +1000

    initial commit

---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class MyFirstComponent extends Component{
+    render() {
+        return (
+            <div>
+                I am content of a child
+            </div>
+        );
+
+    }
+}
+
+export default MyFirstComponent;
